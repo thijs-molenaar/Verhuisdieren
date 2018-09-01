dc6304b2105f708857496445a69f0fc66edde99a
Thijs Molenaar <thijsmolenaar@gmail.com>
2018-09-02 00:01:45 +0200

    Add SearchViewController. Add button to open SearchViewController on initial view.

--- Verhuisdieren/SearchViewController.swift
+++ Verhuisdieren/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  Verhuisdieren
 //
 //  Created by Thijs on 01/09/2018.
@@ -8,25 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var searchForCats: UIButton!
+class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func openSearchForCatsView(_ sender: Any) {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let controller = storyboard.instantiateViewController(withIdentifier: "SearchViewController")
-        self.present(controller, animated: true, completion: nil)
-    }
-    
 }
-
