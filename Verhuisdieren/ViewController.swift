fe0dbec031f4da964922c035be9ee4f259e72a92
Thijs Molenaar <thijsmolenaar@gmail.com>
2018-09-01 23:42:00 +0200

    Initial commit.

---
+++ Verhuisdieren/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Verhuisdieren
+//
+//  Created by Thijs on 01/09/2018.
+//  Copyright © 2018 Thijs Molenaar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
